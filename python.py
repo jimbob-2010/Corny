79f0b0c6a8b44bf51660cf5810ee4331f0e283df
jimbob-2010 <julianheimann9@gmail.com>
2025-09-02 10:48:46 +0000

    update

--- python.py
+++ python.py
@@ -1,1 +1,8 @@
 print("connect")
+lachssystem32 = input("User info: ")
+print(lachssystem32)
+connect = input("passwort: ")
+if connect == "alp":
+    print("helo welkom tu sistem")
+else:
+    print("scut se fak ap")
