46ea6673454bd5998d1abab5b200a07a31b7f3c6
jimbob-2010 <julianheimann9@gmail.com>
2025-09-03 06:58:35 +0000

    abdäit

--- python.py
+++ python.py
@@ -6,3 +6,13 @@ if connect == "alp":
     print("helo welkom tu sistem")
 else:
     print("scut se fak ap")
+print ("stupid")
+lupe = input ("tell me somethink about you")
+print (lupe)
+print ("sell")
+hunter = input("wat is yur name: ")
+if hunter == "julian":
+    print ("hello julian")
+else:
+    print("get your bitchass out of this sistem")
+print ("whats up")
